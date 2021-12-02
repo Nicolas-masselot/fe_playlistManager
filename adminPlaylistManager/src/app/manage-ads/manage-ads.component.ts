3e1a8400ef73d3e3804581842da5bf1717e4163b
nicolas-masselot <nicolasmasselot0@gmail.com>
2021-12-02 14:46:17 +0100

    ajout de la page de gestion des annonces pour les admins et les annonceurs

--- adminPlaylistManager/src/app/manage-ads/manage-ads.component.ts
+++ adminPlaylistManager/src/app/manage-ads/manage-ads.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component , ViewChild } from '@angular/core';
+import {MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { CustomPaginator } from '../customPaginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { MessageService } from '../services/message.service';
+import {ToastrService} from "ngx-toastr";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../services/auth.service';
+
+export interface Advertisement{
+  idAd:number,
+  emailAdvertiser:string,
+  adName:string
+}
 
 @Component({
   selector: 'app-manage-ads',
   templateUrl: './manage-ads.component.html',
-  styleUrls: ['./manage-ads.component.scss']
+  styleUrls: ['./manage-ads.component.scss'],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: CustomPaginator() } 
+  ]
 })
-export class ManageAdsComponent implements OnInit {
+export class ManageAdsComponent implements AfterViewInit {
 
-  constructor() { } // recherche ads = filtre dans la liste des ads à afficher
+  faSearch = faSearch ;
+  colonnes: string[] = ["infosAd","options"];
+  datasource = new MatTableDataSource<Advertisement>(DATA_TEST) ;
+  roleUser: string | undefined ;
+  idUser: number | undefined ;
 
-  ngOnInit(): void {
+  constructor(private service:MessageService,private toastr: ToastrService,private authserv:AuthService) { } // recherche ads = filtre dans la liste des ads à afficher
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit(): void {
+
+    this.paginator._intl.itemsPerPageLabel = "Ads per page :";
+    this.datasource.paginator = this.paginator ;
+    this.idUser = this.authserv.userID;
+    this.roleUser = this.authserv.role ;
+    // this.roleUser = "admin";
+
+    if (this.roleUser == "admin") {
+      this.service.sendMessage('recupAllAds',{}).subscribe(
+        (response)=>{
+          this.datasource.data = response.data ;
+        },
+        (error) => {
+          this.toastr.error("Une erreur s'est produite");
+          console.log(error) ;
+        }
+      );
+    } else {
+      this.service.sendMessage('recupAds',{idAdvertiser: this.idUser}).subscribe(
+        (response)=>{
+          this.datasource.data = response.data ;
+        },
+        (error) => {
+          this.toastr.error("Une erreur s'est produite");
+          console.log(error) ;
+        }
+      );
+    }
+
+  }
+
+  filtrerAds(event:Event){
+    const nomAd= (event.target as HTMLInputElement).value;
+    this.datasource.filter = nomAd.trim().toLowerCase() ;
+  }
+
+  openDialogAds(idAd:number,typeDialog:number){
+    if (typeDialog == 1) {
+      console.log("modifs pour ad "+idAd)
+    } else {
+      console.log("delete ad "+idAd) ;
+    }
   }
 
 }
+
+const DATA_TEST: Advertisement[] = [
+  {idAd:1,emailAdvertiser:"mail1@test.com", adName: "ad1"},
+  {idAd:2,emailAdvertiser:"mail2@test.com", adName: "ad2"},
+  {idAd:69,emailAdvertiser:"mail69@test.com", adName: "ad69"},
+  {idAd:420,emailAdvertiser:"mail420@test.com", adName: "ad420"},
+  {idAd:1,emailAdvertiser:"mail1@test.com", adName: "ad1"},
+  {idAd:2,emailAdvertiser:"mail2@test.com", adName: "ad2"},
+  {idAd:69,emailAdvertiser:"mail69@test.com", adName: "ad69"},
+  {idAd:420,emailAdvertiser:"mail420@test.com", adName: "ad420"},
+  {idAd:1,emailAdvertiser:"mail1@test.com", adName: "ad1"},
+  {idAd:2,emailAdvertiser:"mail2@test.com", adName: "ad2"},
+  {idAd:69,emailAdvertiser:"mail69@test.com", adName: "ad69"},
+  {idAd:420,emailAdvertiser:"mail420@test.com", adName: "ad420"},
+  {idAd:1,emailAdvertiser:"mail1@test.com", adName: "ad1"},
+  {idAd:2,emailAdvertiser:"mail2@test.com", adName: "ad2"},
+  {idAd:69,emailAdvertiser:"mail69@test.com", adName: "ad69"},
+  {idAd:420,emailAdvertiser:"mail420@test.com", adName: "ad420"},
+  {idAd:1,emailAdvertiser:"mail1@test.com", adName: "ad1"},
+  {idAd:2,emailAdvertiser:"mail2@test.com", adName: "ad2"},
+  {idAd:69,emailAdvertiser:"mail69@test.com", adName: "ad69"},
+  {idAd:420,emailAdvertiser:"mail420@test.com", adName: "ad420"},
+]
---
+++ adminPlaylistManager/src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {MessageService, BackendData} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  LoggedIn: boolean | undefined ;
+  role: string | undefined ;
+  userID: number | undefined;
+
+  constructor(private service: MessageService) { }
+
+  sendAuthentification(login: string , password: string): Observable<BackendData> {
+    const requete = {
+      login,
+      password
+    };
+    return this.service.sendMessage('checkLogin', requete); //url temporaire
+  }
+
+  finalizeAuthentification(reponse: BackendData): void {
+    if (reponse.status === 'ok'){
+      this.LoggedIn = true ;
+      this.role = reponse.data.role ;
+      this.userID = reponse.data.userID ;
+    } else {
+      this.LoggedIn = false ;
+      this.role = undefined ;
+    }
+  }
+}
